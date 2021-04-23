93739521e464a418d3a3fddaf5966b7e78dea785
Slugsy <slugsy@slugsy.me>
2021-04-22 20:28:17 -0700

    Updating with latest packages

---
+++ camtron-darwin-x64/camtron.app/Contents/Resources/app/dist/camtron-darwin-x64/camtron.app/Contents/Resources/app/main.js
@@ -0,0 +1,19 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow = null;
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    useContentSize: true,
+    width: 800,
+    height: 600,
+    resizable: false,
+    fullscreen: false
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', () => { mainWindow = null; });
+});
